f64e60490f1e6bb44ac1f48852e75ceff9c36b31
sharhar <wiishahar@gmail.com>
2017-03-01 21:04:38 -0800

    A working compute shader

--- VKraft/Camera.cpp
+++ VKraft/Camera.cpp
@@ -21,9 +21,11 @@ typedef struct VLKComputeContext {
 
 	VkBuffer inBuffer;
 	VkBuffer outBuffer;
+	VkBuffer posBuffer;
 
 	VkDeviceMemory inMemory;
 	VkDeviceMemory outMemory;
+	VkDeviceMemory posMemory;
 
 	VkShaderModule shader;
 	VkDescriptorSetLayout descriptorSetLayout;
@@ -32,6 +34,8 @@ typedef struct VLKComputeContext {
 
 	VkCommandBuffer computeCmdBuffer;
 	VkCommandPool commandPool;
+	VkDescriptorPool descriptorPool;
+	VkDescriptorSet descriptorSet;
 } VLKComputeContext;
 
 VLKComputeContext getComputeContext(VulkanRenderContext* vrc) {
@@ -44,7 +48,7 @@ VLKComputeContext getComputeContext(VulkanRenderContext* vrc) {
 
 	VkBufferCreateInfo inputBufferInfo = {};
 	inputBufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-	inputBufferInfo.size = sizeof(float) * 3 * 3068;
+	inputBufferInfo.size = sizeof(float) * 4 * 3068;
 	inputBufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
 	inputBufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 	inputBufferInfo.queueFamilyIndexCount = 1;
@@ -53,7 +57,7 @@ VLKComputeContext getComputeContext(VulkanRenderContext* vrc) {
 	VLKCheck(vkCreateBuffer(vrc->device.device, &inputBufferInfo, NULL, &context.inBuffer),
 		"Could not create input buffer");
 
-	VkDeviceSize memorySize = 3068 * 3 * sizeof(float);
+	VkDeviceSize memorySize = 3068 * 4 * sizeof(float);
 
 	uint32_t memoryTypeIndex = VK_MAX_MEMORY_TYPES;
 	for (uint32_t k = 0; k < properties.memoryTypeCount; k++) {
@@ -80,7 +84,7 @@ VLKComputeContext getComputeContext(VulkanRenderContext* vrc) {
 	VLKCheck(vkMapMemory(vrc->device.device, context.inMemory, 0, memorySize, 0, &mapped),
 		"Could not map memory");
 
-	memset(mapped, 0, 3068 * 3 * sizeof(float));
+	memset(mapped, 0, 3068 * 4 * sizeof(float));
 
 	vkUnmapMemory(vrc->device.device, context.inMemory);
 
@@ -128,6 +132,48 @@ VLKComputeContext getComputeContext(VulkanRenderContext* vrc) {
 
 	vkBindBufferMemory(vrc->device.device, context.outBuffer, context.outMemory, 0);
 
+	VkBufferCreateInfo posBufferInfo = {};
+	posBufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+	posBufferInfo.size = sizeof(float) * 6;
+	posBufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+	posBufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	posBufferInfo.queueFamilyIndexCount = 1;
+	posBufferInfo.pQueueFamilyIndices = &vrc->device.presentQueueIdx;
+
+	VLKCheck(vkCreateBuffer(vrc->device.device, &posBufferInfo, NULL, &context.posBuffer),
+		"Could not create input buffer");
+
+	memorySize = 6 * sizeof(float);
+
+	memoryTypeIndex = VK_MAX_MEMORY_TYPES;
+	for (uint32_t k = 0; k < properties.memoryTypeCount; k++) {
+		if ((VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT & properties.memoryTypes[k].propertyFlags) &&
+			(VK_MEMORY_PROPERTY_HOST_COHERENT_BIT & properties.memoryTypes[k].propertyFlags) &&
+			(memorySize < properties.memoryHeaps[properties.memoryTypes[k].heapIndex].size)) {
+			memoryTypeIndex = k;
+			break;
+		}
+	}
+
+	assert(memoryTypeIndex != VK_MAX_MEMORY_TYPES);
+
+	memoryAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+	memoryAllocateInfo.pNext = NULL;
+	memoryAllocateInfo.allocationSize = memorySize;
+	memoryAllocateInfo.memoryTypeIndex = memoryTypeIndex;
+
+	VLKCheck(vkAllocateMemory(vrc->device.device, &memoryAllocateInfo, 0, &context.posMemory),
+		"Could not allocate memory");
+
+	VLKCheck(vkMapMemory(vrc->device.device, context.posMemory, 0, memorySize, 0, &mapped),
+		"Could not map memory");
+
+	memset(mapped, 0, 6 * sizeof(float));
+
+	vkUnmapMemory(vrc->device.device, context.posMemory);
+
+	vkBindBufferMemory(vrc->device.device, context.posBuffer, context.posMemory, 0);
+
 	uint32_t codeSize;
 	char* code = new char[20000];
 	HANDLE fileHandle = 0;
@@ -150,7 +196,7 @@ VLKComputeContext getComputeContext(VulkanRenderContext* vrc) {
 
 	delete[] code;
 
-	VkDescriptorSetLayoutBinding bindings[2];
+	VkDescriptorSetLayoutBinding bindings[3];
 	bindings[0].binding = 0;
 	bindings[0].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
 	bindings[0].descriptorCount = 1;
@@ -163,9 +209,15 @@ VLKComputeContext getComputeContext(VulkanRenderContext* vrc) {
 	bindings[1].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
 	bindings[1].pImmutableSamplers = NULL;
 
+	bindings[2].binding = 2;
+	bindings[2].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+	bindings[2].descriptorCount = 1;
+	bindings[2].stageFlags = VK_SHADER_STAGE_COMPUTE_BIT;
+	bindings[2].pImmutableSamplers = NULL;
+
 	VkDescriptorSetLayoutCreateInfo setLayoutCreateInfo = {};
 	setLayoutCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-	setLayoutCreateInfo.bindingCount = 2;
+	setLayoutCreateInfo.bindingCount = 3;
 	setLayoutCreateInfo.pBindings = bindings;
 
 	VLKCheck(vkCreateDescriptorSetLayout(vrc->device.device, &setLayoutCreateInfo, NULL, &context.descriptorSetLayout),
@@ -200,6 +252,101 @@ VLKComputeContext getComputeContext(VulkanRenderContext* vrc) {
 	VLKCheck(vkCreateComputePipelines(vrc->device.device, NULL, 1, &pipelineCreateInfo, NULL, &context.pipeline),
 		"Failed to create pipeline");
 
+	VkDescriptorPoolSize descriptorPoolSize = {};
+	descriptorPoolSize.type = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+	descriptorPoolSize.descriptorCount = 3;
+
+	VkDescriptorPoolCreateInfo descriptorCreateInfo = {};
+	descriptorCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+	descriptorCreateInfo.pNext = NULL;
+	descriptorCreateInfo.flags = 0;
+	descriptorCreateInfo.maxSets = 1;
+	descriptorCreateInfo.poolSizeCount = 1;
+	descriptorCreateInfo.pPoolSizes = &descriptorPoolSize;
+
+	VLKCheck(vkCreateDescriptorPool(vrc->device.device, &descriptorCreateInfo, 0, &context.descriptorPool),
+		"Failed to create discriptor pool");
+
+	VkDescriptorSetAllocateInfo descriptorSetAllocateInfo = {};
+	descriptorSetAllocateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+	descriptorSetAllocateInfo.pNext = NULL;
+	descriptorSetAllocateInfo.descriptorPool = context.descriptorPool;
+	descriptorSetAllocateInfo.descriptorSetCount = 1;
+	descriptorSetAllocateInfo.pSetLayouts = &context.descriptorSetLayout;
+
+	VLKCheck(vkAllocateDescriptorSets(vrc->device.device, &descriptorSetAllocateInfo, &context.descriptorSet), 
+		"Failed to create descriptor set");
+
+	VkDescriptorBufferInfo inDescriptorBuffer = {};
+	inDescriptorBuffer.buffer = context.inBuffer;
+	inDescriptorBuffer.offset = 0;
+	inDescriptorBuffer.range = VK_WHOLE_SIZE;
+
+	VkDescriptorBufferInfo outDescriptorBuffer = {};
+	outDescriptorBuffer.buffer = context.outBuffer;
+	outDescriptorBuffer.offset = 0;
+	outDescriptorBuffer.range = VK_WHOLE_SIZE;
+
+	VkDescriptorBufferInfo posDescriptorBuffer = {};
+	posDescriptorBuffer.buffer = context.posBuffer;
+	posDescriptorBuffer.offset = 0;
+	posDescriptorBuffer.range = VK_WHOLE_SIZE;
+
+	VkWriteDescriptorSet writeDescriptorSet[3] = {};
+	writeDescriptorSet[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+	writeDescriptorSet[0].pNext = NULL;
+	writeDescriptorSet[0].dstSet = context.descriptorSet;
+	writeDescriptorSet[0].dstBinding = 0;
+	writeDescriptorSet[0].dstArrayElement = 0;
+	writeDescriptorSet[0].descriptorCount = 1;
+	writeDescriptorSet[0].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+	writeDescriptorSet[0].pBufferInfo = &inDescriptorBuffer;
+	writeDescriptorSet[0].pImageInfo = NULL;
+	writeDescriptorSet[0].pTexelBufferView = NULL;
+
+	writeDescriptorSet[1].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+	writeDescriptorSet[1].pNext = NULL;
+	writeDescriptorSet[1].dstSet = context.descriptorSet;
+	writeDescriptorSet[1].dstBinding = 1;
+	writeDescriptorSet[1].dstArrayElement = 0;
+	writeDescriptorSet[1].descriptorCount = 1;
+	writeDescriptorSet[1].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+	writeDescriptorSet[1].pBufferInfo = &outDescriptorBuffer;
+	writeDescriptorSet[1].pImageInfo = NULL;
+	writeDescriptorSet[1].pTexelBufferView = NULL;
+
+	writeDescriptorSet[2].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+	writeDescriptorSet[2].pNext = NULL;
+	writeDescriptorSet[2].dstSet = context.descriptorSet;
+	writeDescriptorSet[2].dstBinding = 2;
+	writeDescriptorSet[2].dstArrayElement = 0;
+	writeDescriptorSet[2].descriptorCount = 1;
+	writeDescriptorSet[2].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+	writeDescriptorSet[2].pBufferInfo = &posDescriptorBuffer;
+	writeDescriptorSet[2].pImageInfo = NULL;
+	writeDescriptorSet[2].pTexelBufferView = NULL;
+
+	vkUpdateDescriptorSets(vrc->device.device, 3, writeDescriptorSet, 0, 0);
+
+	VkCommandPoolCreateInfo commandPoolCreateInfo = {};
+	commandPoolCreateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+	commandPoolCreateInfo.pNext = NULL;
+	commandPoolCreateInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+	commandPoolCreateInfo.queueFamilyIndex = vrc->device.presentQueueIdx;
+
+	VLKCheck(vkCreateCommandPool(vrc->device.device, &commandPoolCreateInfo, NULL, &context.commandPool), 
+		"Failed to create command pool");
+
+	VkCommandBufferAllocateInfo commandBufferAllocateInfo = {};
+	commandBufferAllocateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+	commandBufferAllocateInfo.pNext = NULL;
+	commandBufferAllocateInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+	commandBufferAllocateInfo.commandPool = context.commandPool;
+	commandBufferAllocateInfo.commandBufferCount = 1;
+
+	VLKCheck(vkAllocateCommandBuffers(vrc->device.device, &commandBufferAllocateInfo, &context.computeCmdBuffer),
+		"Failed to create command pool");
+
 	return context;
 }
 
@@ -208,6 +355,15 @@ static void cameraThreadRun(GLFWwindow* win, VulkanRenderContext* vrc) {
 	Vec3* pvecs = NULL;
 
 	VLKComputeContext context = getComputeContext(vrc);
+
+	VkFenceCreateInfo fenceCreateInfo = {};
+	fenceCreateInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+	fenceCreateInfo.pNext = NULL;
+	fenceCreateInfo.flags = 0;
+
+	VkFence fence;
+	VLKCheck(vkCreateFence(vrc->device.device, &fenceCreateInfo, NULL, &fence),
+		"Failed ot create fence");
 
 	while (!glfwWindowShouldClose(win)) {
 		
@@ -233,20 +389,101 @@ static void cameraThreadRun(GLFWwindow* win, VulkanRenderContext* vrc) {
 			vecs[i] = { closeCubes[i]->m_pos.x, closeCubes[i]->m_pos.y, closeCubes[i]->m_pos.z };
 		}
 
-		Vertex* verts = new Vertex[cbsz];
+		if (cbsz > 0) {
+			float* posData = new float[6];
+			posData[0] = 0;
+			posData[1] = 4.3f;
+			posData[2] = 0;
+			posData[3] = 0;
+			posData[4] = 0;
+			posData[5] = 0;
 
-		for (int i = 0; i < cbsz; i++) {
-			verts[i].x = vecs[i].x;
-			verts[i].y = vecs[i].y - 0.5f;
-			verts[i].z = vecs[i].z;
+			void *mapped;
+			vkMapMemory(vrc->device.device, context.posMemory, 0, VK_WHOLE_SIZE, 0, &mapped);
 
-			verts[i].tx = closeCubes[i]->tex->x;
-			verts[i].ty = closeCubes[i]->tex->y;
-			verts[i].tz = closeCubes[i]->tex->z;
-			verts[i].rinf = closeCubes[i]->vid;
+			memcpy(mapped, posData, 6 * sizeof(float));
+
+			VkMappedMemoryRange memoryRange = {};
+			memoryRange.sType = VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE;
+			memoryRange.memory = context.posMemory;
+			memoryRange.offset = 0;
+			memoryRange.size = VK_WHOLE_SIZE;
+			vkFlushMappedMemoryRanges(vrc->device.device, 1, &memoryRange);
+
+			vkUnmapMemory(vrc->device.device, context.posMemory);
+
+			float* inputData = new float[4*cbsz];
+
+			for (int i = 0; i < cbsz;i++) {
+				inputData[i * 4 + 0] = vecs[i].x;
+				inputData[i * 4 + 1] = vecs[i].y;
+				inputData[i * 4 + 2] = vecs[i].z;
+				inputData[i * 4 + 3] = (float)closeCubes[i]->vid;
+			}
+
+			vkMapMemory(vrc->device.device, context.inMemory, 0, VK_WHOLE_SIZE, 0, &mapped);
+
+			memcpy(mapped, inputData, cbsz * 4 * sizeof(float));
+
+			memoryRange.sType = VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE;
+			memoryRange.memory = context.inMemory;
+			memoryRange.offset = 0;
+			memoryRange.size = VK_WHOLE_SIZE;
+			vkFlushMappedMemoryRanges(vrc->device.device, 1, &memoryRange);
+
+			vkUnmapMemory(vrc->device.device, context.inMemory);
+
+			delete[] posData;
+			delete[] inputData;
+
+			VkCommandBufferBeginInfo commandBufferBeginInfo = {};
+			commandBufferBeginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+			commandBufferBeginInfo.pNext = NULL;
+			commandBufferBeginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+			commandBufferBeginInfo.pInheritanceInfo = NULL;
+
+			VLKCheck(vkBeginCommandBuffer(context.computeCmdBuffer, &commandBufferBeginInfo),
+				"Failed to begin command buffer");
+
+			vkCmdBindPipeline(context.computeCmdBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, context.pipeline);
+
+			vkCmdBindDescriptorSets(context.computeCmdBuffer, VK_PIPELINE_BIND_POINT_COMPUTE,
+				context.pipelineLayout, 0, 1, &context.descriptorSet, 0, 0);
+
+			vkCmdDispatch(context.computeCmdBuffer, cbsz, 1, 1);
+
+			VLKCheck(vkEndCommandBuffer(context.computeCmdBuffer),
+				"Failed to end command buffer");
+
+			VkSubmitInfo submitInfo = {};
+			submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+			submitInfo.pNext = NULL;
+			submitInfo.pSignalSemaphores = NULL;
+			submitInfo.signalSemaphoreCount = 0;
+			submitInfo.pWaitSemaphores = NULL;
+			submitInfo.waitSemaphoreCount = 0;
+			submitInfo.pWaitDstStageMask = NULL;
+			submitInfo.commandBufferCount = 1;
+			submitInfo.pCommandBuffers = &context.computeCmdBuffer;
+
+			VLKCheck(vkQueueSubmit(context.computeQueue, 1, &submitInfo, fence),
+				"Failed to submit Queue");
+
+			vkWaitForFences(vrc->device.device, 1, &fence, VK_TRUE, UINT64_MAX);
+			vkResetFences(vrc->device.device, 1, &fence);
+			
+			vkMapMemory(vrc->device.device, context.outMemory, 0, VK_WHOLE_SIZE, 0, &mapped);
+
+			float* data = (float*)mapped;
+
+			for (int i = 0; i < cbsz; i++) {
+				if (data[i] != 4.3f) {
+					std::cout << "Wrong\n";
+				}
+			}
+
+			vkUnmapMemory(vrc->device.device, context.outMemory);
 		}
-
-		//Ray cast using Vulkan Compute shader
 
 		while (Camera::fence > 0) {
 			std::this_thread::sleep_for(std::chrono::microseconds(10));
@@ -258,20 +495,6 @@ static void cameraThreadRun(GLFWwindow* win, VulkanRenderContext* vrc) {
 		Camera::possSize = cbsz;
 
 		Camera::fence = 0;
-
-		/*
-		if (ids != NULL) {
-			delete[] ids;
-		}
-
-		if (infs != NULL) {
-			delete[] infs;
-		}
-
-		if (verts != NULL) {
-			delete[] verts;
-		}
-		*/
 
 		if (pvecs != NULL) {
 			delete[] pvecs;
